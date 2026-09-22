f3423d119af04e65fac19c61100a039037f2ee91
Md. Abdul Latif Siyam <159998231+MdALSiyam@users.noreply.github.com>
2026-09-22 21:31:03 +0600

    Basic Routing, Dynamic Routes & Client Components

--- Part-1/app/page.js
+++ Part-1/app/page.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <main style={{ padding: '20px', fontFamily: 'sans-serif' }}>
-      <h1>Welcome to Next.js Level 1 🚀</h1>
+      <h1>Welcome to Next.js Level 1</h1>
       <p>This is the Home Page.</p>
       
       <Link href="/about">Go to About Page ➡️</Link>
